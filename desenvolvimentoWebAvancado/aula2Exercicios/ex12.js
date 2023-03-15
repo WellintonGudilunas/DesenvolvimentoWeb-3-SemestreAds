bbf66218b79042de10c7d73dda9b68a8d9df6a8a
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-03-15 00:57:50 -0300

    ..

--- desenvolvimentoWebAvancado/aula2Exercicios/ex12.js
+++ desenvolvimentoWebAvancado/aula2Exercicios/ex12.js
@@ -1,14 +1,16 @@
 /*
 Ex 12. Escreva um algoritmo que receba o número do mês e mostre o mês correspondente. Valide mês inválido.
 */
+const prompt = require('prompt-sync')();
 const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-let mesEscolhido = 13;
-
+let mesEscolhido = Number(prompt("Digite o número do Mês desejado: "));
 
 for (let i = 0; i < meses.length; i++){
     if(mesEscolhido < 1 || mesEscolhido > 12){
-      console.log(`O número ${mesEscolhido} é inválido`);
+      console.log(`O número ${mesEscolhido} é inválido.`);
        break;
-    } 
-    console.log(meses[mesEscolhido - 1]);
-}
+    } else {
+      console.log(meses[mesEscolhido - 1]);
+      break;
+    }
+  }
--- desenvolvimentoWebAvancado/aula2Exercicios/ex13.js
+++ desenvolvimentoWebAvancado/aula2Exercicios/ex13.js
@@ -3,10 +3,14 @@ Ex 13. Escreva um algoritmo que leia valores REAIS nas variáveis A e B e o tipo
 tipo CARACTERE. Imprima o resultado da operação de A por B se o operador aritmético for válido; caso contrário
 deve ser impresso uma mensagem de operador não definido. Tratar erro de divisão por zero.
 */
+const prompt = require('prompt-sync')();
+
 function main(){
-  let a = 20, b = 20;
   let operadores = ["+", "-", "*", "/", "**"];
-  let operador = "+";
+  let a = Number(prompt("Digite o primeiro número: "));
+  let b = Number(prompt("Agora, digite o segundo número: "));
+  
+  let operador = prompt("Agora, digite o OPERADOR: ");
 
 
     if(operadores.indexOf(operador) != -1){
@@ -23,7 +27,7 @@ function main(){
       }
     }
     const operacao = a + operador + b;
-    console.log(eval(operacao));
+    console.log(`O resutado da soma de ${a} + ${b} é: ${eval(operacao)}`);
     
 }
 main();
