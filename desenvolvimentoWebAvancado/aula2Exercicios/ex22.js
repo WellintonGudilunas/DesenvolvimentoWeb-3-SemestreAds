5355ee114957e4847385fab3d8ff224468593ba1
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-03-16 23:39:53 -0300

    Update ex22.js

--- desenvolvimentoWebAvancado/aula2Exercicios/ex22.js
+++ desenvolvimentoWebAvancado/aula2Exercicios/ex22.js
@@ -2,9 +2,7 @@
 Ex 2. Construa um programa que leia um vetor de 5 elementos e calcule a média destes valores. Na seqüência, 
 apresente na tela os valores que são iguais ou superiores à média.
 */
-
 const prompt = require('prompt-sync')();
-
 let arrayNumerico = [];
 for(let index = 0; index < 5; index++){
     let numDigitado = Number(prompt("Digite a nota " + (index + 1) + ": "));
