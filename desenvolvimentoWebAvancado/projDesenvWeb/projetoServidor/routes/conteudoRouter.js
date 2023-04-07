22f7bbc0dd30c4cc99ca9de53840d092b92f2a7a
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-04-07 13:20:02 -0300

    ...

--- desenvolvimentoWebAvancado/projDesenvWeb/projetoServidor/routes/conteudoRouter.js
+++
@@ -1,8 +0,0 @@
-const conteudoController = require('../controllers/conteudoController');
-const express = require("express"); //importando o express
-
-const router = express.Router();
-router.get("/", conteudoController.listar);
-router.post('/', conteudoController.salvar);
-
-module.exports = router;
